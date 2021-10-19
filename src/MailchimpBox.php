7c6bf912dde40ae3199475a75332693c0b1827af
coulterpeterson <coulter@maplespaceinc.com>
2021-10-19 12:02:57 -0700

    Starting to add Mailchimp logic

--- src/MailchimpBox.php
+++ src/MailchimpBox.php
@@ -2,12 +2,43 @@
 
 namespace Coulterpeterson\MailchimpBox;
 
+use NZTim\Mailchimp;
+use Illuminate\Support\Facades\Log;
+
 class MailchimpBox{
 
-    public static function event(string $input1, int $input2, ?string $input3 = null)
+    public static function subscribe(string $email, string $firstName, string $lastName, 
+        string $audienceName, string $tagToApply)
     {
         // send the event to mailchimp api
-        return $input1 . ' - ' . $input2;
+        //return $input1 . ' - ' . $input2;
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            Log::error('Invalid email provided.');
+            return;
+        }
+
+        $email = strtolower($email);
+
+        // Todo: Get audience ID from audience name
+
+            // Then subscribe member to given list
+
+            // Then add given tag to that member
+
     }
+
+    private static function get_audience_id( string $audienceName )
+    {
+
+    }
+
+    private static function get_location_prefix_for_api( string $apiKey ) 
+    {
+        $subStrings = explode( '-', $apiKey );
+
+        return $subStrings[1];
+    }
+
 
 }
